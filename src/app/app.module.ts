d049f9091a3f81e9b22e49e7b2cd2001e794058c
Ismael <ismaelaraujoo@gmail.com>
2020-11-21 20:57:43 -0300

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { CreateComponent } from './components/create/create.component';
+import { TaskComponent } from './components/task/task.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    CreateComponent,
+    TaskComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AppModule { }
---
+++ src/app/components/create/create.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { TaskmanagerService } from 'src/app/service/taskmanager.service';
+import { Task } from 'src/app/model/task.model';
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.css']
+})
+export class CreateComponent implements OnInit {
+  formCadastro;
+  sucesso: boolean = false;
+  constructor(
+    private service: TaskmanagerService,
+    private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.formCadastro = this.fb.group({
+      titulo: [''],
+      descricao: ['']
+    });
+  }
+
+  cadastro(){
+    this.service.criarTask(this.formCadastro.value).subscribe(
+      (task: Task) => {
+        this.sucesso = true;
+        this.service.emitChange("");
+        console.log(this);
+      }, (error: any) => {
+        console.log( error);
+      }
+    )
+  }
+
+  closeModal(){
+    this.sucesso = false;
+    this.formCadastro = this.fb.group({
+      titulo: [''],
+      descricao: ['']
+    });
+  }
+
+  /**
+  showChildModal():void {
+    this.formCadastro.
+  }
+  
+  hideChildModal():void {
+    this.childModal.hide();
+   }
+  }
+  **/
+
+}
